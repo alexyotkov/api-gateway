2df0997c06415209eb7a767f831fb4c043861d42
Aleksandar Yotkov <a.p.yotkov-1@student.tudelft.nl>
2024-10-07 20:37:44 +0200

    Added gateway routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const serviceRoutes = require('./routes/service.route');
 const userRoutes = require('./routes/user.route');
+const gatewayRoutes = require('./routes/gateway.route');
 
 const app = express();
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(helmet());
+app.use('/api', gatewayRoutes);
 app.use('/', serviceRoutes);
 app.use('/auth', userRoutes);
 app.listen(PORT, () => {
